be31d3c8f6873f97f665c581a5c7a0559ab2ee32
HyebinPark96 <96033225+HyebinPark96@users.noreply.github.com>
2023-07-24 20:18:33 +0900

    주석 수정

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -16,12 +16,14 @@ def solution(prices):
         price = queue.popleft()
         
         for i in queue:
+            # !! 1초 후 바로 떨어지더라도 최소 1초간 떨어지지 않은 것으로 보기 때문에 비교 전 +1 먼저 해줘야 함   
             cnt += 1
             
+            # !! 떨어지는 순간 해당 반복문 빠져나오기 (중첩반복문이므로 상위 반복문의 다음 인덱스 반복 시작) 
             if i < price:
                 break
         
-        # cnt가 0이라면 1초만 가격이 떨어지지 않은 것으로 보며, 
+        # cnt가 1이라면 1초만 가격이 떨어지지 않은 것으로 보며, 
         # 마지막 원소인 경우, 0초간 가격이 떨어지지 않은 것으로 봐야 하므로 마지막 원소는 조건에서 제외  
         if cnt == 1 and (len(answer) != len(prices) - 1):
             answer.append(1)
