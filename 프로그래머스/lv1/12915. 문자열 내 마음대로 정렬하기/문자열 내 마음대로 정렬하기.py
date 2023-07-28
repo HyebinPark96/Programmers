cad14169d74077dc1b872c5f4e2da64861aa0473
HyebinPark96 <96033225+HyebinPark96@users.noreply.github.com>
2023-07-28 22:31:16 +0900

    Update 문자열 내 마음대로 정렬하기.py

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -4,23 +4,19 @@ def solution(strings, n):
     # 문자열의 인덱스 n번째 글자 추출 
     target_alphabet = []
 
-    tmp = []
     for target in strings:
         target_alphabet.append(target[n])
-        tmp.append(target)
         
     # 오름차순 정렬
     target_alphabet.sort()
     # 사전순으로 앞선 문자열이 앞쪽에 위치하도록 복사해둔 문자열 배열 오름차순 미리 정렬해두기
-    tmp.sort()
+    strings.sort()
     
     for alphabet in target_alphabet:
-        for string in tmp:
+        for string in strings:
             if alphabet == string[n]:
                 answer.append(string)
-                tmp.remove(string)
+                strings.remove(string)
                 break
 
-            
-    
     return answer
