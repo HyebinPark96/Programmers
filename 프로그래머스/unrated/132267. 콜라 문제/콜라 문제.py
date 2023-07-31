ff95afe1e9d734849497cc6054816ed205f1a7b7
HyebinPark96 <96033225+HyebinPark96@users.noreply.github.com>
2023-07-31 22:19:35 +0900

    Update 콜라 문제.py

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -6,11 +6,7 @@ def solution(a, b, n):
     cnt = 0
     
     while n // a > 0: # 더이상 나눠지지 않을 때까지 반복
-        if n % a == 0:
-            cnt += b * (n // a)
-            n = n - (n - (n % a)) + (b * (n // a)) # 준 병 빼고, 받은 병 더하기 
-        else: 
-            cnt += b * (n // a)
-            n = n - (n - (n % a)) + (b * (n // a)) # 준 병 빼고, 받은 병 더하기 
+        cnt += b * (n // a)
+        n = n - (n - (n % a)) + (b * (n // a)) # 준 병 빼고, 받은 병 더하기 
 
     return cnt
