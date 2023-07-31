fc82ff706f415fc7104f0a951bb51decdd00475c
HyebinPark96 <96033225+HyebinPark96@users.noreply.github.com>
2023-07-31 22:17:21 +0900

    Update 콜라 문제.py

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -5,16 +5,12 @@ def solution(a, b, n):
     # 빈 병 카운팅
     cnt = 0
     
-    # 안나눠지는 병 카운팅 (한꺼번에 모아서 가장 마지막에 나눌 것)
-    tmp = 0
-    
     while n // a > 0: # 더이상 나눠지지 않을 때까지 반복
         if n % a == 0:
             cnt += b * (n // a)
             n = n - (n - (n % a)) + (b * (n // a)) # 준 병 빼고, 받은 병 더하기 
         else: 
             cnt += b * (n // a)
-            tmp += math.floor(n % a) # 나머지는 모아두기
             n = n - (n - (n % a)) + (b * (n // a)) # 준 병 빼고, 받은 병 더하기 
 
     return cnt
